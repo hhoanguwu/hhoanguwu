5abdcc924383ceac683f2c177196c8ee1b277b64
hhoanguwu <138114333+hhoanguwu@users.noreply.github.com>
2023-06-29 17:33:34 +0700

    Add files via upload

---
+++ sus.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+#include<iomanip>
+#include<cmath>
+using namespace std;
+int main(){
+    char c;
+    cin >>c;
+    if(c>=656&&c<=90){
+        cout <<"la chu hoa";
+    }else {
+    cout <<"ko phai chu hoa";
+    }
+}
